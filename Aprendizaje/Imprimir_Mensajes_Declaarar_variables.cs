42b5261c6ea853d0d4206fb88b6153bf10a40130
Luis Carlos Rincón <125197117+LuisCarlosRinconG@users.noreply.github.com>
2024-10-18 16:12:35 -0500

    Create Imprimir_Mensajes_Declaarar_variables.cs
    Imprimir mensajes y declarar variables en C#

---
+++ Aprendizaje/Imprimir_Mensajes_Declaarar_variables.cs
@@ -0,0 +1,13 @@
+//imprimir mensaje con salto de linea
+Console.WriteLine("Hola mundo");
+//imprimir mensaje sin salto de linea
+Console.Write("Hello World");
+//Declaración de variables
+char a='1';
+int num=12;
+decimal dec=12.2m;
+double num1=12.2;
+bool b=true;
+string frase="esta es una frase";
+
+Console.Write(frase);
