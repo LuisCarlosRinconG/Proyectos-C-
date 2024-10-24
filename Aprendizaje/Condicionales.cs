f5f17f0a7fe8ebe4e2b4bc3429cbcd177555fba1
Luis Carlos Rincón <125197117+LuisCarlosRinconG@users.noreply.github.com>
2024-10-24 14:45:59 -0500

    Create Condicionales.cs
    Uso de condicionales

---
+++ Aprendizaje/Condicionales.cs
@@ -0,0 +1,32 @@
+// Your code goes here
+//Ejercicio planteado por https://learn.microsoft.com para el aprendizaje de C#
+
+Random random = new Random();
+int daysUntilExpiration = random.Next(12);
+int discountPercentage = 0;
+
+if (daysUntilExpiration>=10 && daysUntilExpiration>5)
+{
+    Console.WriteLine("Your subscription will expire soon. Renew now!");
+}
+else if (daysUntilExpiration>=5 && daysUntilExpiration>1)
+{
+    Console.WriteLine(@$"Your subscription expires in {daysUntilExpiration} days.
+                        Renew now and save 10%!");
+}
+else if(daysUntilExpiration==1)
+{
+    Console.WriteLine("Your subscription expires within a day!");
+    discountPercentage+=20;
+    Console.WriteLine($"Renew now and save {discountPercentage}%.");
+}
+else if (daysUntilExpiration==0)
+{
+    Console.WriteLine("Your subscription has expired.");
+    discountPercentage+=30;
+    Console.WriteLine($"Renew now and save {discountPercentage}%.");
+}
+else
+{
+    Console.WriteLine("\n");
+}
